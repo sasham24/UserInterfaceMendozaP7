a67e6683a76ac1b35d0ec1ab252dad4141ab7925
sasham24 <736149@student.dusd.net>
2024-04-26 15:27:02 -0700

    game over text

--- P7UserInterfaceMendoza/Assets/Scripts/GameManager.cs
+++ P7UserInterfaceMendoza/Assets/Scripts/GameManager.cs
@@ -1,16 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+
 
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
-    private float spawnRate = 1.0f; 
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+    public bool isGameActive;
+    private int score;
+    private float spawnRate = 1.0f;
+    
 
     // Start is called before the first frame update
     void Start()
     {
+        isGameActive = true;
         StartCoroutine(SpawnTarget());
+
+        score = 0;
+        UpdateScore(0);
+
     }
 
     // Update is called once per frame
@@ -20,11 +32,22 @@ public class GameManager : MonoBehaviour
     }
     IEnumerator SpawnTarget()
     {
-        while (true)
+        while (isGameActive)
         {
             yield return new WaitForSeconds(spawnRate);
             int index = Random.Range(0, targets.Count);
-            Instantiate(targets[index]); 
+            Instantiate(targets[index]);
+
         }
+    }
+    public void UpdateScore(int scoreToAdd)
+    {
+        score = scoreToAdd;
+        scoreText.text = "Score: " + score;
+    }
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
+        isGameActive = false;
     }
 }
