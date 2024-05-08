ccccd7aa7607bf936e51385a858db1b6dc361fc9
sasham24 <736149@student.dusd.net>
2024-05-08 15:26:48 -0700

    finished
    challenge

--- P7UserInterfaceMendoza/Assets/Scripts/GameManager.cs
+++ P7UserInterfaceMendoza/Assets/Scripts/GameManager.cs
@@ -16,6 +16,12 @@ public class GameManager : MonoBehaviour
     public bool isGameActive;
     private int score;
     private float spawnRate = 1.0f;
+
+    public TextMeshProUGUI livesText;
+    private int lives;
+
+    public GameObject pauseScreen;
+    private bool paused;
     
 
     // Start is called before the first frame update
@@ -27,7 +33,10 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            ChangePaused();
+        }
     }
     IEnumerator SpawnTarget()
     {
@@ -41,8 +50,17 @@ public class GameManager : MonoBehaviour
     }
     public void UpdateScore(int scoreToAdd)
     {
-        score = scoreToAdd;
+        score += scoreToAdd;
         scoreText.text = "Score: " + score;
+    }
+    public void UpdateLives(int livesToChange)
+    {
+        lives += livesToChange;
+        livesText.text = "Lives: " + lives;
+        if (lives <= 0)
+        {
+            GameOver();
+        }
     }
     public void GameOver()
     {
@@ -62,7 +80,23 @@ public class GameManager : MonoBehaviour
         
         StartCoroutine(SpawnTarget());
         UpdateScore(0);
+        UpdateLives(3);
 
         titleScreen.gameObject.SetActive(false);
+    }
+    void ChangePaused()
+    {
+        if (!paused)
+        {
+            paused = true;
+            pauseScreen.SetActive(true);
+            Time.timeScale = 0;
+        }
+        else
+        {
+            paused = false;
+            pauseScreen.SetActive(false);
+            Time.timeScale = 1;
+        }
     }
 }
